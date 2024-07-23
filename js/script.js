0f869de430e671c8d34c0484af2752fe39a773df
MarceloFlopez <marceloferreiralopez@gmail.com>
2024-07-23 09:01:22 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+    .addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        btn.value = 'Enviando...';
+
+        const serviceID = 'default_service';
+        const templateID = 'template_dsqhi3b';
+
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.value = 'INSCREVA-SE';
+                alert('Enviado!');
+            }, (err) => {
+                btn.value = 'INSCREVA-SE';
+                alert(JSON.stringify(err));
+            });
+    });
